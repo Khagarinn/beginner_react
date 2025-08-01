7cc2f19f4f9da9d7a69137b9640691c9f1aca7d3
Mert Can <mertcanguvennn@gmail.com>
2025-08-01 13:33:01 +0300

    added footer

--- beginner_tutorial/src/App.tsx
+++ beginner_tutorial/src/App.tsx
@@ -1,13 +1,14 @@
 import Header from "./Header"
+import Footer from "./Footer";
 
 function App() {
   // this will serve as the root of the application
   // we will add components here later
   return (
-    <Header>
-      
-    </Header>
-
+    <>
+      <Header />
+      <Footer />
+    </>
   );
 }
 
