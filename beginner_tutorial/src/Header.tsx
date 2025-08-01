c2f87b341d45db6b8fb16a431257d87bda6d739e
Mert Can <mertcanguvennn@gmail.com>
2025-08-01 13:16:01 +0300

    made a header, description and a hyperlink styled list

---
+++ beginner_tutorial/src/Header.tsx
@@ -0,0 +1,19 @@
+function Header() {
+    return (
+        <header>
+            <h1>Welcome to the Beginner React Tutorial</h1>
+            <p>Learn the basics of React step by step.</p>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+}
+
+export default Header;
