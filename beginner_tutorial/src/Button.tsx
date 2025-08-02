8b38e2bf17d89ef3df0500ec9f961fd5dd42a382
Mert Can <mertcanguvennn@gmail.com>
2025-08-02 14:12:02 +0300

    button_stuff: Add button click functionality

--- beginner_tutorial/src/Button.tsx
+++ beginner_tutorial/src/Button.tsx
@@ -1,5 +1,13 @@
 function Button() {
-    return(<button className="button_default">Click Me!</button>);
+
+const handleClick = () => {
+    alert("Button was clicked! Check the console for more details.");
+}
+const handleClick2 = (name: string) => {
+  console.log(name + ' clicked me');
+}
+
+    return(<button className="button_default" onClick={() => handleClick2("admin")}>Click Me!</button>);
 }
 
 export default Button;
