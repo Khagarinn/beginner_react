2cff2917e27221a798f8269f2a12a34b482eaf93
ArdaAkyaz <ardaakyaz@hotmail.com>
2025-08-02 18:56:24 +0300

    tiklama

--- beginner_tutorial/src/Button.tsx
+++ beginner_tutorial/src/Button.tsx
@@ -1,5 +1,11 @@
-function Button() {
-    return(<button className="button_default">Click Me!</button>);
+function Button() 
+{
+    let count = 0;
+    const tiklama = () => {
+        count++;
+        console.log("Button clicked " + count + " times");
+    };
+    return(<button className="button_default" onClick={() => tiklama()}>Click Me!</button>);
 }
 
 export default Button;
